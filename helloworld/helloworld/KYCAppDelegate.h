16c3b55e290224ec9adc82174b4f3c9e8022fec9
kycjson <lanou3g@kangdaxia.local>
2014-06-10 16:43:01 +0800

    helloworld
    项目第一天

---
+++ helloworld/helloworld/KYCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KYCAppDelegate.h
+//  helloworld
+//
+//  Created by 康延昌 on 14-6-10.
+//  Copyright (c) 2014年 康延昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ helloworld/helloworld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by 康延昌 on 14-6-10.
+//  Copyright (c) 2014年 康延昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KYCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KYCAppDelegate class]));
+    }
+}
